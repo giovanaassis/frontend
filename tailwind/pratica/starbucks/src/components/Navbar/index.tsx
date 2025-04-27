06b076498f0da5d6edace4628fc369d22ecda3c9
giovanaassis <giovana_2011_11@hotmail.com>
2025-04-27 01:01:06 -0300

    feat: finished navBar

--- tailwind/pratica/starbucks/src/components/Navbar/index.tsx
+++ tailwind/pratica/starbucks/src/components/Navbar/index.tsx
@@ -1,21 +1,23 @@
-import Logo from "@/assets/logo.png"
-import { MapPinIcon } from "@heroicons/react/24/solid"
+import Logo from "@/assets/logo.png";
+import { MapPinIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  const navItemStyles = `cursor-pointer relative py-1 rounded-full w-30 flex items-center justify-center overflow-hidden transition duration-[.5s] hover:text-black hover:font-normal z-1`;
+
   return (
     <nav className="flex items-center gap-x-10 p-5">
-        <img src={Logo} alt="logo" className="w-10"/>
-        <ul className="flex gap-10 uppercase font-light">
-            <li>coffee</li>
-            <li>tea</li>
-            <li>gift cards</li>
-            <div className="flex gap-1">
-                <MapPinIcon className="size-5"/>
-                <li>find a store</li>
-            </div>
-        </ul>
+      <img src={Logo} alt="logo" className="w-10" />
+      <ul className="flex gap-10 font-light uppercase">
+        <li className={`${navItemStyles} afterStyles showButton`}>coffee</li>
+        <li className={`${navItemStyles} afterStyles showButton`}>tea</li>
+        <li className={`${navItemStyles} afterStyles showButton`}>gift cards</li>
+        <div className={`${navItemStyles} w-40 gap-2 afterStyles showButton`}>
+          <MapPinIcon className="size-5" />
+          <li>find a store</li>
+        </div>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
