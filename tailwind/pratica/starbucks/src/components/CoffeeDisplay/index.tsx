681e3502a9940a9a82af5354f7bc130b8b2f052f
giovanaassis <giovana_2011_11@hotmail.com>
2025-06-14 19:01:40 -0300

    feat: [tailwind] update on starbucks

--- tailwind/pratica/starbucks/src/components/CoffeeDisplay/index.tsx
+++ tailwind/pratica/starbucks/src/components/CoffeeDisplay/index.tsx
@@ -29,7 +29,7 @@ const CoffeeDisplay = ({ selectedCoffee }: CoffeeDisplayProps) => {
           initial={{ opacity: "0%", x: -50 }}
           animate={{ opacity: "50%", x: 0 }}
           exit={{ opacity: "0%", x: -50 }}
-          transition={{ delay: 0.3, duration }}
+          transition={{ duration }}
         >
           {selectedCoffee.name}
         </motion.span>
