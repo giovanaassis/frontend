1088db4e4ec548934eb6afe4919848e414dcf9cb
giovanaassis <giovana_2011_11@hotmail.com>
2026-05-10 21:36:28 -0300

    feat [nextjs]: add new project bento-grids

---
+++ nextjs/pratica/bento-grids/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center p-20 bg-gray-900 tracking-[.20em]">
+      <p>
+        This is a bunch of little layouts to practice Grid CSS. All taken from
+        Bento Grids.
+      </p>
+    </div>
+  );
+}
